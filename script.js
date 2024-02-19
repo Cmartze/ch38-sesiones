7d738d1df192d0ca6e1d2d4f1e118f6be68f9c5b
carlos MartineZ <101781martz@gmail.com>
2024-02-19 13:26:18 -0600

    bottons

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-consol.log("hola ch38"); 
+consol.log("Hola ch38"); 
+
+//* funcion que envie un saludo con alert.
+
+function enviarSaludo(){
+    alert("holakase");
+}
+
+function enviarSaludoAPersona() {
+    let message = prompt("Hola usuario dime tu nombre");
+    alert(`Hola ${message}`);
+}
